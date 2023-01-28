bb37274acb9b7fae833bc8627240eaeb12e71c97
jjtt48 <74917684+jjtt48@users.noreply.github.com>
2023-01-28 15:56:33 -0500

    Add files via upload

---
+++ 0x00-Python_Introduction/0x00-Python_introduccion.py
@@ -0,0 +1,8 @@
+# Entrega actividad 0
+nombre = "Juan Jose Toro Trujillo"
+fecha = "23/01/2023"
+hora = "16:15"
+
+print("Hola Mundo")
+print("Mi nombre es "+ nombre)
+print("Hoy es " + fecha +" y son las " + hora)
